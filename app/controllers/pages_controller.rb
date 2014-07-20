ea0c02ebde616957c2a02110bb7ac1d268eba92d
Garrett Martin <garrettqmartin8@optimum.net>
2014-07-20 12:12:04 -0400

    add streak to profile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,7 @@ class PagesController < AuthenticatedController
     @post_today = current_user.posts.today
     @posts = current_user.posts
     @points = @posts.sum(:points)
+    @current_streak = current_user.streak
   end
 
   def archive
