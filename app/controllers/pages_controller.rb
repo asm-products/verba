f57a28137dd5e840fc7fb19d954f7e4cdb7c04d5
Garrett Martin <garrettqmartin8@optimum.net>
2014-09-08 13:16:04 -0500

    remove writing prompt controller action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,16 +7,13 @@ class PagesController < AuthenticatedController
   def profile
     @post_today = current_user.posts.today
     @posts = current_user.posts
+    # FIXME should be a column on user model
     @points = @posts.sum(:points)
     @current_streak = current_user.streak
   end
 
   def archive
     @posts = current_user.posts.order("created_at DESC")
-  end
-
-  def new_prompt
-    session[:prompt] = WritingPrompt.all.pluck(:prompt).sample
   end
 
   def download
