2afe2626f2bd52d26c9c22c6bb0e907a1a72eb39
Garrett Martin <garrettqmartin8@optimum.net>
2014-10-11 14:25:46 -0500

    fix streak method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,17 +11,18 @@ class User < ActiveRecord::Base
   def streak
     return if posts.blank?
     @posts = posts.order("created_at DESC")
-    previous = @posts.first.created_at.day
-    @posts.pluck(:created_at).map(&:day).slice_before do |current|
-      previous, previous2 = current, previous
-      previous2 - 1 != previous
-    end.to_a.max.count
+    days = @posts.pluck(:created_at).map(&:day)
+
+    days.each_with_index.inject(1) do |streak, (day, index)|
+      streak += 1 if day - 1 == days[index + 1]
+      streak
+    end
   end
 
   def increment_longest_streak
-    streak_number = streak
-    if streak_number >= longest_streak
-      update_attribute(:longest_streak, streak_number)
+    current_streak = streak
+    if current_streak >= longest_streak
+      update_attribute(:longest_streak, current_streak)
     end
   end
 
