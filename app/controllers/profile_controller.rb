4eaca1570c8b49ceff5daef0038dd0e8351cb650
Garrett Martin <garrettqmartin8@optimum.net>
2014-10-11 16:24:04 -0500

    allow users of the #streak method specify any association

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,8 +1,8 @@
 class ProfileController < ApplicationController
   def show
     @post_today = current_user.posts.today
-    @posts = current_user.posts 
+    @posts = current_user.posts
     @points = current_user.points
-    @current_streak = current_user.streak
+    @current_streak = current_user.streak(:posts)
   end
 end
--- app/models/concerns/has_streak.rb
+++ app/models/concerns/has_streak.rb
@@ -1,8 +1,8 @@
 module HasStreak
-  def streak
-    return if posts.blank?
-    @posts = posts.order("created_at DESC")
-    days = @posts.pluck(:created_at).map(&:day)
+  def streak(association)
+    return if self.send(association).blank?
+    @association = self.send(association).order("created_at DESC")
+    days = @association.pluck(:created_at).map(&:day)
 
     days.each_with_index.inject(1) do |streak, (day, index)|
       streak += 1 if day - 1 == days[index + 1]
