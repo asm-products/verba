c778ee89ebea7c31cb167d247cb11b90d7aca783
Garrett Martin <me@garrettqmartin.com>
2015-02-26 08:37:23 -0700

    Add null object for reminder copy.

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -3,7 +3,7 @@ class ReminderMailer < ActionMailer::Base
 
   def send_reminder(user)
     @user = user
-    @reminder_copy = ReminderCopy.copy_for(user)
-    mail(to: user.email, subject: @reminder_copy.try(:subject))
+    @reminder_copy = ReminderCopy.copy_for(user) || DefaultReminderCopy.new
+    mail(to: user.email, subject: @reminder_copy.subject)
   end
 end
---
+++ app/models/default_reminder_copy.rb
@@ -0,0 +1,13 @@
+class DefaultReminderCopy
+  def subject
+    "Daily writing reminder from Verba"
+  end
+
+  def action
+    "Start writing."
+  end
+
+  def copy
+    "Keep it up! Habits are powerful."
+  end
+end
