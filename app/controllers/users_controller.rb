7c7afaa06e1f1dfc39d67b19bea20058abfe296a
Alicia <alicia@icalialabs.com>
2015-03-01 23:21:19 -0600

    Adds show action on users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+    @published_posts = @user.posts.published
+    @total_posts = @user.posts
+    @points = @user.points
+    @current_streak = @user.streak(:posts)
+  end
 
   def create
     @user = User.new(user_params)
