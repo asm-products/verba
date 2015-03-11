3eba13f4326176263ad07526e1c69350b0b9bdbc
Garrett Martin <me@garrettqmartin.com>
2015-03-10 20:30:43 -0500

    Use url parameters before current_user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = current_user || User.find(params[:id])
+    @user = User.find(params[:id]) || current_user
     @profile = Profile.new(@user, view_context)
   end
 
