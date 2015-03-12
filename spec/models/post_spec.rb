bd5fb35d3a1bec18b53fe30723b7ca01ebb799f5
Garrett Martin <me@garrettqmartin.com>
2015-03-12 16:59:31 -0500

    Add spec for ensuring posts have a word_count after saving.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 describe Post do
+  describe "creating a post" do
+    context "after_save" do
+      let(:post) { create(:post, content: "Hello world.") }
+
+      it "should update word_count" do
+        expect(post.word_count).to eq(2)
+      end
+    end
+  end
 end
