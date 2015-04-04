59ffbf4012af22fe03098d1e6561c66fd4b9d9bb
Garrett Martin <me@garrettqmartin.com>
2015-04-03 20:47:40 -0500

    Add barebones admin page for writing prompts.

--- app/controllers/admin/prompts_controller.rb
+++ app/controllers/admin/prompts_controller.rb
@@ -1,5 +1,19 @@
 class Admin::PromptsController < AdminController
   def index
-    @prompts = Prompt.all
+    @prompts = Prompt.all.order(used: :asc)
+    @prompt = Prompt.new
+  end
+
+  def create
+    @prompt = Prompt.new(prompt_params)
+    @prompt.save!
+
+    redirect_to admin_prompts_path
+  end
+
+  private
+
+  def prompt_params
+    params.require(:prompt).permit(:content)
   end
 end
