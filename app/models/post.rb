7e4c2c99dd0fc45bc37c5528632f290cc040275c
Garrett Martin <me@garrettqmartin.com>
2015-04-17 21:35:30 -0500

    Add correct day logic.

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,11 @@ class Post < ActiveRecord::Base
   include PgSearch
   pg_search_scope :search_by_content, against: :content
 
-  scope :published, -> { where(published: true) }
-  scope :latest_post, -> { order("created_at desc").first || NullPost.new }
+  scope :published,     -> { where(published: true) }
+  scope :latest_post,   -> { order("created_at desc").first || NullPost.new }
+  scope :on_date, -> (day) { where("created_at >= ? AND created_at <= ?",
+                             day.beginning_of_day,
+                             day.end_of_day) }
 
   def date(without_year: false)
     if without_year
