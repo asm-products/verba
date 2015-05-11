ef2477d6c50cc626acde0641567b61f348f933e9
Garrett Martin <me@garrettqmartin.com>
2015-05-11 11:18:02 -0500

    Change mailer host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "verba-app.com" }
+  config.action_mailer.default_url_options = { host: "writeverba.com" }
 
 
   ActionMailer::Base.smtp_settings = {
