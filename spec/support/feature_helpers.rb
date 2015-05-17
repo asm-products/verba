452e92c73048ac8e891ffcbce9938182f1ecdbe0
Garrett Martin <me@garrettqmartin.com>
2015-05-16 22:59:10 -0500

    Fix specs.

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -1,6 +1,6 @@
 module FeatureHelpers
   def sign_in_as(user)
-    visit root_path
+    visit login_path
     fill_in "username", with: user.username
     fill_in "password", with: "12345678"
     click_button "Login"
@@ -8,7 +8,7 @@ module FeatureHelpers
 
   def register_user
     visit root_path
-    click_link "Register"
+    click_link "landing-page-register-button"
     fill_in "username", with: "garrettqmartin"
     fill_in "email", with: "garrett@example.com"
     fill_in "password", with: "12345678"
