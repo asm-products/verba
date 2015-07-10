c6e84acb69025a1f29b16c0aed4d793d5e4fe165
Garrett Martin <me@garrettqmartin.com>
2015-07-09 23:18:36 -0500

    Add better flash message design.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,15 @@ module ApplicationHelper
     end
     collection.to_json
   end
+
+  def flash_message
+    flash_message_types = %w(notice error success)
+    flash.map do |k, v|
+      if flash_message_types.include?(k)
+        content_tag(:div, class: "flash-message--#{k}") do
+          content_tag(:div, "x", class: "flash-message__close") + v
+        end
+      end
+    end.join.html_safe
+  end
 end
