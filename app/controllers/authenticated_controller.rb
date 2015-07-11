c147f608a3679317320179e83a8196ea180e705c
Garrett Martin <me@garrettqmartin.com>
2015-07-10 22:50:52 -0500

    Move flash message and add feature conditional to controller.

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,4 +1,4 @@
 class AuthenticatedController < ApplicationController
   before_action :require_user
-  before_action :require_subscriber
+  before_action :require_subscriber, if: Proc.new { feature_active?(:payments) }
 end
